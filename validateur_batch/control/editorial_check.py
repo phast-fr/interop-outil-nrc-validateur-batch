091c2cb3e4b28acbe459f54bf19264b009ded3c0
Pierre-Olivier Grégoire <pierre-olivier.gregoire@phast.fr>
2026-02-11 16:15:36 +0100

    bs3 : differentiate bs3-struct, bs3-term and bs3-part ar4-fr : actually call the check function

--- validateur_batch/control/editorial_check.py
+++ validateur_batch/control/editorial_check.py
@@ -419,7 +419,7 @@ def _check_bs10(df: pd.DataFrame, pt: pd.Series, syn: pd.Series) -> pd.DataFrame
 
     idx_lower_limb_syn = df.loc[syn & 
                  (df.loc[:, "FSN"].str.contains("lower limb", regex=False, case=False))
-                 & (~df.loc[:, "term"].str.contains(r"\b(membre inférieur|membrum inferius|membri inferioris)\b", regex=True, case=False))].index # noqa
+                 & (~df.loc[:, "term"].str.contains(r"\b(?:membre(?: (?:droit|gauche)) inférieur|membrum inferius|membri inferioris)\b", regex=True, case=False))].index # noqa
 
 
     idx_lower_leg = df.loc[pt
@@ -452,7 +452,7 @@ def _check_bs11(df: pd.DataFrame, pt: pd.Series, syn: pd.Series) -> pd.DataFrame
 
     idx_limb_syn = df.loc[syn &
                  (df.loc[:, "FSN_no_sem"].str.contains("upper limb", regex=False, case=False))
-                 & (~df.loc[:, "term"].str.contains(r"\b(membre supérieur|membrum superius|membri superioris)\b", regex=True, case=False))].index # noqa  
+                 & (~df.loc[:, "term"].str.contains(r"\b(?:membre supérieur|membrum superius|membri superioris)\b", regex=True, case=False))].index # noqa  
 
     idx_pt_upper_arm_not_ok = df.loc[pt
                            & (df.loc[:, "FSN_no_sem"].str.contains("upper arm", regex=False, case=False)) # noqa
